284610fcb3b0b6966ba2657e9549399c69a19f76
Emma Gerigscott <elgerig@gmail.com>
2022-06-14 08:33:01 -0700

    dropdown about sidebar fix internal links, still bug with dropdown on sub pages

---
+++ script.js
@@ -0,0 +1,15 @@
+const dropdown = document.getElementsByClassName("dropdown-about");
+let i;
+
+
+for(i = 0; i < dropdown.length; i++) {
+    dropdown[i].addEventListener("click", function() {
+        this.classList.toggle('active');
+        let dropdownContent = this.nextElementSibling;
+        if (dropdownContent.style.display === 'block') {
+            dropdownContent.style.display = 'none';
+        } else {
+            dropdownContent.style.display = 'block';
+        }
+    });
+}

1740229e1752f620511888ad03815c95dd5fe129
Emma Gerigscott <elgerig@gmail.com>
2022-06-15 16:15:23 -0700

    trying to make a slideshow lol

--- script.js
+++ script.js
@@ -13,3 +13,24 @@ for(i = 0; i < dropdown.length; i++) {
         }
     });
 }
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName('img-box');
+    if (n > slides.length) {slideIndex = 1}
+    if (n< 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display='none';
+    }
+}
